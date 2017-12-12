daffb1939b61a344f552c39e9020092833faf711
Mohamedismail77 <mohamedismail77@yahoo.com>
2017-12-12 17:20:55 +0200

    nk

--- app/src/main/java/com/example/m_ismail/myapplication/Player.java
+++ app/src/main/java/com/example/m_ismail/myapplication/Player.java
@@ -1,18 +1,30 @@
 package com.example.m_ismail.myapplication;
 
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.source.smoothstreaming.SsChunkSource;
+import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
 
 public class Player extends AppCompatActivity {
 
@@ -29,14 +41,27 @@ public class Player extends AppCompatActivity {
         TrackSelector trackSelector =
                 new DefaultTrackSelector(videoTrackSelectionFactory);
 
-//      2. Create the player
+        //2. Create the player
         SimpleExoPlayer player =
                 ExoPlayerFactory.newSimpleInstance(this, trackSelector);
 
 
+        // Measures bandwidth during playback. Can be null if not required.
+// Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "MyApplication"), (TransferListener<? super DataSource>) bandwidthMeter);
+// Produces Extractor instances for parsing the media data.
+
+        HlsMediaSource videoSource = new HlsMediaSource(Uri.parse("http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8"),
+                dataSourceFactory, null, null);
+// Prepare the player with the source.
+        player.prepare(videoSource);
+
 
         SimpleExoPlayerView simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.exo_player);
         simpleExoPlayerView.setPlayer(player);
+
+
 
     }
 }
